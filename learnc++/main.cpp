027907b7b3c751a7ad6d921c0b8b00bdf113b896
Henry <bubbman@icloud.com>
2020-06-10 16:20:32 +0800

    Initial Commit

---
+++ learnc++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learnc++
+//
+//  Created by Henry on 2020/6/10.
+//  Copyright © 2020 Henry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
